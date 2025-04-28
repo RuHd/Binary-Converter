5636c2bbed5105ab770e91fed7accb34545aa5c9
RuHd <ruaan.ram@gmail.com>
2025-04-28 15:08:13 -0300

    Responsive Design & Drop Numbers Effects mobile perfomance adjustment

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import "./page.css";
 import arrows from "../assets/Arrows.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { convertToBinary, convertToDecimal } from "./utils/functions";
 import Dropnumber from "./components/Dropnumber";
 
@@ -11,12 +11,26 @@ import Dropnumber from "./components/Dropnumber";
 
 export default function Home() {
   const [insertValue, setinsertValue] = useState(0)
+  const [appWidth, setappWidth] = useState(0)
   const [result, setresult] = useState("")
 
-  let quantity = 200 // Establish the quantity of numbers to be dropped
+  let quantity = appWidth <= 830 ? 50 : 200 // Establish the quantity of numbers to be dropped based on the screen size for better performance
 
   let arr = Array.from({ length: quantity }, (_, i) => i + 1);
 
+  useEffect(() => {
+    const handleSize = () => {
+      setappWidth(window.innerWidth)
+    }
+
+    window.addEventListener("resize", handleSize)
+  
+    return () => {
+      window.removeEventListener("resize", handleSize)
+    }
+  }, [])
+  
+  console.log(appWidth)
   return (
     <div className={"page"}>
       {
@@ -35,7 +49,7 @@ export default function Home() {
           </h1>
           <form>
             <label>
-              <input placeholder="Type Here...." onChange={(e) => setinsertValue(e.target.value)} defaultValue={null}/>
+              <input placeholder="Type Here...." onChange={(e) => setinsertValue(e.target.value)} defaultValue={null} type="number"/>
             </label>
             <label>
               <input placeholder="Result Goes Here" readOnly value={result}/>
